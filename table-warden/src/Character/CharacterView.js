5a03c34538404913e9e59ac04c0898dce5d24915
Kuturin <agrabska1994@gmail.com>
2025-02-16 21:29:39 +0100

    Updating dependecies

--- table-warden/src/Character/CharacterView.js
+++ table-warden/src/Character/CharacterView.js
@@ -6,12 +6,12 @@ const CharacterView = ({ characters }) => {
   const { id } = useParams();
   const character = characters.find(character => character.id === parseInt(id));
 
-  if (!character) {
+  if (!character) { // If the character is not found, display a message
     return <div>Character not found</div>;
   }
 
-  return (
-    <div>
+  return ( // Display the character's details
+    <div> 
       <h1>Character Details</h1>
       <p>Name: {character.name}</p>
       <p>Age: {character.age}</p>
@@ -20,7 +20,7 @@ const CharacterView = ({ characters }) => {
       <p>Status: {character.isDead ? 'Dead' : 'Alive'}</p>
       {character.isDead && <p>Date of Death: {character.dateOfDeath}</p>}
       <div>
-        <Link to="/list">Back to List</Link>
+        <Link to="/list">Back to List</Link> 
       </div>
     </div>
   );
